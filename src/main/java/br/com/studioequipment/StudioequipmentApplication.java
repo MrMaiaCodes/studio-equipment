617f9558316269b4f84165cf26d2eb9d150f1e8a
Silvio Maia <ingles.silvio@gmail.com>
2023-04-01 08:41:59 -0300

    project added to github repository

---
+++ src/main/java/br/com/studioequipment/StudioequipmentApplication.java
@@ -0,0 +1,13 @@
+package br.com.studioequipment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudioequipmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudioequipmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/studioequipment/StudioequipmentApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.studioequipment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudioequipmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
